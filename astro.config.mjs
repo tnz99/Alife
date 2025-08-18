eecda53f6f931ce329a0a16ce5c74cf04f6ce95c
tnz99 <tenzintsomoo.66@gmail.com>
2025-08-18 17:02:58 +0600

    initial

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.com/tnz99/Alife',
-	base: '/my-astro-site/', 
+	site: 'https://tnz99.github.io/blog',
+	base: '/blog/',
 	integrations: [mdx(), sitemap()],
 });
